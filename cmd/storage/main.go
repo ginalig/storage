fa193b9a5cf2022f715f8483e94e2b2ac76a1780
ginalig <aanigay@edu.hse.ru>
2022-09-09 18:41:35 +0300

    Simple upload method

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/ginalig/storage/internal/storage"
 )
 
 func main() {
 	st := storage.NewStorage()
-	fmt.Println("Hello world", st)
+
+	file, err := st.Upload("text.txt", []byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Hello world", file)
 }
